10fd297b9f5a9d242b9de582ae69b268c5877897
pkl1024 <56228279+pkl1024@users.noreply.github.com>
2020-07-28 15:31:10 +0800

    添加Idea配置文件

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
-public class BubbleSort {
+class BubbleSort {
+    public static int[] bubbleSort(int[] nums) {
+        for (int i=0;i<nums.length;i++){
+            for (int j=0;j<nums.length-1;j++){
+                if (nums[j]>nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
 }
--- src/Demo.java
+++ src/Demo.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Demo {
+    public static void main(String[] args) {
+        int[] n = {5,30,25,48,19,1,0,99,201};
+        int[] nums = BubbleSort.bubbleSort(n);
+        for (int a:nums) {
+            System.out.println(a);
+        }
+    }
 }
