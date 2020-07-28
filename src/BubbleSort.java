ef06e427283c7b264b49c2b3d0006a88f08643d0
pkl1024 <56228279+pkl1024@users.noreply.github.com>
2020-07-28 18:08:26 +0800

    单修改冒泡排序

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -2,15 +2,20 @@
  * 冒泡排序
  */
 class BubbleSort {
-    public static int[] bubbleSort(int[] nums) {
+    public static int[] bubbleSort(int[] nums) throws InterruptedException {
         for (int i=0;i<nums.length-1;i++){
             for (int j=0;j<nums.length-1-i;j++){
-                System.out.println(j);
                 if (nums[j]>nums[j+1]){
                     int temp = nums[j];
                     nums[j] = nums[j+1];
                     nums[j+1] = temp;
                 }
+
+                /*for (int a:nums){
+                    System.out.print(a+" ");
+                }
+                System.out.println();
+                Thread.sleep(1000);*/
             }
         }
         return nums;
