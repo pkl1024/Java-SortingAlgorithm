a3f332a1b0d8e5f14c38061e40ebc25bf839998b
pkl1024 <56228279+pkl1024@users.noreply.github.com>
2020-08-13 09:38:09 +0800

    简单更新

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -2,6 +2,12 @@
  * 冒泡排序
  */
 class BubbleSort {
+    /**
+     *
+     * @param nums 需要排序的数组
+     * @return 排序完成的数组
+     * @throws InterruptedException
+     */
     public static int[] bubbleSort(int[] nums) throws InterruptedException {
         for (int i=0;i<nums.length-1;i++){
             for (int j=0;j<nums.length-1-i;j++){
